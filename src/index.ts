f3ca95828bf0c77d0aea09dbe44a506b668ba88d
deekshasharma <deeksha.sharma25@gmail.com>
2022-02-23 15:36:51 +0530

    Adds types for error and data

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
-const fs = require('fs');
-const toml = require('toml');
-const path =
-  '/Users/deekshasharma/Desktop/mindgrep/config-loader/src/collect.toml';
+// const fs = require('fs');
+// const toml = require('toml');
+// const path =
+//   '/Users/deekshasharma/Desktop/mindgrep/config-loader/src/collect.toml';
+//
+// fs.readFile(path, 'utf8', (err, data) => {
+//   if (err) {
+//     // console.error(err)
+//     return;
+//   }
+//   const data2 = toml.parse(data);
+//   const json = {
+//     collect: JSON.parse(JSON.stringify(data2))
+//   };
+//   // console.log(json);
+// });
 
-fs.readFile(path, 'utf8', (err, data) => {
-  if (err) {
-    // console.error(err)
-    return;
-  }
-  const data2 = toml.parse(data);
-  const json = {
-    collect: JSON.parse(JSON.stringify(data2))
-  };
-  // console.log(json);
-});
+import { convertTomlToJson } from './tomlToJson';
+convertTomlToJson();
--- src/tomlToJson.ts
+++ src/tomlToJson.ts
@@ -1,23 +1,20 @@
-// const toml = require('toml');
-// const fs = require('fs');
-// const path = '/Users/deekshasharma/Desktop/mindgrep/config-loader/src/collect.toml';
-//
-// function convertTomlToJson() {
-//     fs.readFile(
-//         path,
-//         'utf8',
-//         (err, data) => {
-//             if (err) {
-//                 // console.error(err)
-//                 return;
-//             }
-//             const data2 = toml.parse(data);
-//             const json = {
-//                 collect: JSON.parse(JSON.stringify(data2))
-//             };
-//             console.log(json);
-//         }
-//     );
-// }
-//
-// module.exports = { convertTomlToJson }
+import ErrnoException = NodeJS.ErrnoException;
+
+const toml = require('toml');
+const fs = require('fs');
+const path =
+  '/Users/deekshasharma/Desktop/mindgrep/config-loader/src/collect.toml';
+
+export const convertTomlToJson = () => {
+  fs.readFile(path, 'utf8', (err: ErrnoException | null, data: string) => {
+    if (err) {
+      // console.error(err)
+      return;
+    }
+    const data2 = toml.parse(data);
+    const json = {
+      collect: JSON.parse(JSON.stringify(data2))
+    };
+    console.log(json);
+  });
+};
