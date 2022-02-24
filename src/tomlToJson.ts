dd98ed13cb107c76540f627bac00528ac1fb1058
deekshasharma <deeksha.sharma25@gmail.com>
2022-02-24 21:34:29 +0530

    remove unused callback

--- src/tomlToJson.ts
+++ src/tomlToJson.ts
@@ -2,11 +2,7 @@ import ErrnoException = NodeJS.ErrnoException;
 const toml = require('toml'); //TODO: Use import instead of require
 const fs = require('fs'); //TODO: Use import instead of require
 
-export const convertTomlToJson = async (
-  path: string,
-  fileName: string,
-  callback?: (data: any) => void
-) => {
+export const convertTomlToJson = async (path: string, fileName: string) => {
   return new Promise((resolve, reject) => {
     fs.readFile(path, (err: ErrnoException | null, data: string) => {
       if (err) reject(`Error handling file, ${err}`);
