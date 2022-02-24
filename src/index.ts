f0a1f4ad07dafbaa9a35a82238fb621058b33249
deekshasharma <deeksha.sharma25@gmail.com>
2022-02-24 11:22:42 +0530

    reads JSON file and log

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
-import { convertTomlToJson } from './tomlToJson';
-convertTomlToJson();
+import { convertTomlToJson, readJSONFile } from './tomlToJson';
+const path =
+  '/Users/deekshasharma/Desktop/mindgrep/config-loader/src/collect.toml';
+
+const jsonFilePath =
+  '/Users/deekshasharma/Desktop/mindgrep/config-loader/src/sample.json';
+// convertTomlToJson(path);
+readJSONFile(jsonFilePath);
--- src/tomlToJson.ts
+++ src/tomlToJson.ts
@@ -2,10 +2,8 @@ import ErrnoException = NodeJS.ErrnoException;
 
 const toml = require('toml');
 const fs = require('fs');
-const path =
-  '/Users/deekshasharma/Desktop/mindgrep/config-loader/src/collect.toml';
 
-export const convertTomlToJson = () => {
+export const convertTomlToJson = (path: string) => {
   fs.readFile(path, 'utf8', (err: ErrnoException | null, data: string) => {
     if (err) {
       // console.error(err)
@@ -16,5 +14,19 @@ export const convertTomlToJson = () => {
       collect: JSON.parse(JSON.stringify(data2))
     };
     console.log(json);
+  });
+};
+
+export const readJSONFile = (path: string) => {
+  fs.readFile(path, 'utf8', (err: ErrnoException | null, data: string) => {
+    if (err) {
+      // console.error(err)
+      return;
+    }
+    const data2 = JSON.parse(data);
+    // const json = {
+    //   fileName: JSON.parse(JSON.stringify(data2))
+    // };
+    console.log(data2);
   });
 };
