90ad66343b82014f2ec5d509e1d44047072ca208
deekshasharma <deeksha.sharma25@gmail.com>
2022-02-24 12:10:00 +0530

    derives fileName and file extension

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import { convertTomlToJson, readJSONFile } from './tomlToJson';
+import path from 'path';
 
-const path =
+/**
+ * Read TOML file
+ */
+const tomlFilePath =
   '/Users/deekshasharma/Desktop/mindgrep/config-loader/src/collect.toml';
-const fileName = 'collect';
+const extensionToml = path.extname(tomlFilePath);
+const tomlFileName = path.basename(tomlFilePath, extensionToml);
+
+convertTomlToJson(tomlFilePath, tomlFileName);
+
+/**
+ * Read JSON file
+ */
 const jsonFilePath =
   '/Users/deekshasharma/Desktop/mindgrep/config-loader/src/sample.json';
-
-convertTomlToJson(path, fileName);
-readJSONFile(jsonFilePath, 'sample');
+const extensionJson = path.extname(jsonFilePath);
+const jsonFileName = path.basename(jsonFilePath, extensionJson);
+readJSONFile(jsonFilePath, jsonFileName);
