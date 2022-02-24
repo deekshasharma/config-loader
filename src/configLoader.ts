b5c80ee7084ece38f38057a852b9edbda486218c
hhimanshu <harit.himanshu@yahoo.in>
2022-02-24 12:12:17 +0530

    feat: clean code to include only one function

--- src/configLoader.ts
+++ src/configLoader.ts
@@ -1,20 +1,6 @@
 import FileHound from 'filehound';
-import yaml from 'js-yaml';
-import fs from 'fs';
-
-export const yaml2Json = () => {
-  const inputfile = 'infra.yaml',
-    //outputfile = 'output.json',
-    obj = yaml.load(fs.readFileSync(inputfile, { encoding: 'utf-8' }));
-  // eslint-disable-next-line no-console
-  console.log(JSON.stringify(obj));
-  // this code if you want to save
-  //fs.writeFileSync(outputfile, JSON.stringify(obj, null, 2));
-};
 
 export const findConfigFiles = (path = './') => {
-  // eslint-disable-next-line no-console
-  console.log(`finding files in ${path}`);
   const getJsonFiles = () =>
     FileHound.create().paths(path).match('*.json').find();
   const getYamlFiles = () =>
