3b41ca46734753b33e237035b4d65df774023d68
deekshasharma <deeksha.sharma25@gmail.com>
2022-02-24 15:45:55 +0530

    get all toml and json files

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,30 @@
 import { convertTomlToJson, readJSONFile } from './tomlToJson';
 import path from 'path';
+import { findConfigFiles } from './configLoader';
 
 /**
  * Read TOML file
  */
-const tomlFilePath =
-  '/Users/deekshasharma/Desktop/mindgrep/config-loader/src/collect.toml';
-const extensionToml = path.extname(tomlFilePath);
-const tomlFileName = path.basename(tomlFilePath, extensionToml);
-
-convertTomlToJson(tomlFilePath, tomlFileName);
+// const tomlFilePath =
+//   '/Users/deekshasharma/Desktop/mindgrep/config-loader/src/collect.toml';
+// const extensionToml = path.extname(tomlFilePath);
+// const tomlFileName = path.basename(tomlFilePath, extensionToml);
+// convertTomlToJson(tomlFilePath, tomlFileName);
 
 /**
  * Read JSON file
  */
-const jsonFilePath =
-  '/Users/deekshasharma/Desktop/mindgrep/config-loader/src/sample.json';
-const extensionJson = path.extname(jsonFilePath);
-const jsonFileName = path.basename(jsonFilePath, extensionJson);
-readJSONFile(jsonFilePath, jsonFileName);
+// const jsonFilePath =
+//   '/Users/deekshasharma/Desktop/mindgrep/config-loader/src/sample.json';
+// const extensionJson = path.extname(jsonFilePath);
+// const jsonFileName = path.basename(jsonFilePath, extensionJson);
+// readJSONFile(jsonFilePath, jsonFileName);
+
+findConfigFiles('./src').then((allFiles) => {
+  const jsonFiles = allFiles.json;
+  const tomlFiles = allFiles.toml;
+  const yamlFiles = allFiles.yaml;
+  console.log(`JSON Files ->`, jsonFiles);
+  console.log(`TOML Files ->`, tomlFiles);
+  console.log(`YAML Files ->`, yamlFiles);
+});
